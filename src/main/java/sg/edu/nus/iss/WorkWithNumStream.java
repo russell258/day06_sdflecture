fa8d8b2c2869a17a6dd779a2858ba2891be6a6df
russell258 <duckie258@gmail.com>
2023-05-11 14:35:05 +0800

    number stream using function mapping

--- src/main/java/sg/edu/nus/iss/WorkWithNumStream.java
+++ src/main/java/sg/edu/nus/iss/WorkWithNumStream.java
@@ -26,18 +26,36 @@ public class WorkWithNumStream {
         br.close();
         fr.close();
 
-        Consumer<String> c = (String v) -> {
-            System.out.printf("v = %s\n", v);
+        // Consumer<String> c = (String v) -> {
+        //     System.out.printf("v = %s\n", v);
+        // };
 
-        };
+        //from the stream of strings, map them to Integer and filter for <0, then print forEach of them.
         numList.stream()
-            .map(c -> Integer.parseInt(c))
+            .map((String c) -> Integer.parseInt(c))
             .filter((Integer c) -> (c<0))
             .forEach((Integer c) -> {
-                System.out.printf("V = %d\n", c);
+                System.out.printf("c = %d\n", c);
             }); 
         
+        // from the stream of strings, map them into integer and filter for <0, then make them into a list.
+        List<Integer> intList = numList.stream()
+            .map((String v) -> Integer.parseInt(v))
+            .filter((Integer v) -> (v<0))
+            .toList();
 
+        System.out.println("intList: " + intList);
+
+        //from the number stream, map each string into an int
+        // then filter for those that are <0
+        // reduce gives identity as 0 (usually initiate i=0), then use the operator SUM, with integer i.
+        // sum + i so just add all the streams coming in.
+        int total = numList.stream()
+            .map ((String v) -> Integer.parseInt(v))
+            .filter ((Integer v) -> (v<0))
+            .reduce(0, (Integer sum, Integer i) -> sum+i);
+
+            System.out.println("total: "+total);
 
     }
 }
